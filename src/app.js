7d0c68b61cdf9e48dd45df276ddb5d5599997b1a
sneelyg <santiagoneely@gmail.com>
2022-07-19 01:07:46 +0000

    funcionando, falta estilos

--- src/app.js
+++ src/app.js
@@ -7,21 +7,37 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  var excuseGen = () => {
+    var quien = [
+      "Mi mamá ",
+      "Mi Perro ",
+      "El payaso ",
+      "Cada perro ",
+      "El búho "
+    ];
+    var quehizo = ["meó ", "se comió ", "quemó ", "cocinó "];
+    var quecosa = [
+      "el auto ",
+      "la cocina ",
+      "mi tarea ",
+      "al perro ",
+      "un caballo "
+    ];
+    var cuando = [
+      "ayer.",
+      "después de clase.",
+      "mientras estaba en el baño.",
+      "cuando pasó el temblor."
+    ];
 
-  var quien = ["Mi mamá ", "Mi Perro ", "El payaso ", "Cada perro ", "El búho "];
-  var quehizo= ["meó ", "se comió ", "quemó ", "cocinó "];
-  var quecosa = ["el auto ", "la casa ", "mi tarea ", "al perro ", "un caballo "];
-  var cuando =["ayer.", "después de clase.", "mientras estaba en el baño.", "cuando pasó el temblor."];
+    var excusa =
+      quien[Math.floor(Math.random() * quien.length)] +
+      quehizo[Math.floor(Math.random() * quehizo.length)] +
+      quecosa[Math.floor(Math.random() * quecosa.length)] +
+      cuando[Math.floor(Math.random() * cuando.length)];
 
-  var excusa = quien[(Math.floor(Math.random()*(quien.length+1))] +
-  quehizo[(Math.floor(Math.random()*(quehizo.length+1))] +
-  quecosa[(Math.floor(Math.random()*(quecosa.length+1))] +
-  cuando[(Math.floor(Math.random()*(cuando.length+1))];
-
-  console.log(excusa);
-
-
-
-document.querySelector(#excuse).innerHTML = excusa;
-
+    console.log(excusa);
+    return excusa;
+  };
+  document.querySelector("#excuse").innerHTML = excuseGen();
 };
