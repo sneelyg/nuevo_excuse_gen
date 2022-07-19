e33d24cd45d9f1878eb5d5650c6643b0c138451e
sneelyg <santiagoneely@gmail.com>
2022-07-19 00:59:15 +0000

    çavanzando

--- src/app.js
+++ src/app.js
@@ -8,7 +8,20 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
 
-  var quien = [];
+  var quien = ["Mi mamá ", "Mi Perro ", "El payaso ", "Cada perro ", "El búho "];
+  var quehizo= ["meó ", "se comió ", "quemó ", "cocinó "];
+  var quecosa = ["el auto ", "la casa ", "mi tarea ", "al perro ", "un caballo "];
+  var cuando =["ayer.", "después de clase.", "mientras estaba en el baño.", "cuando pasó el temblor."];
 
-  console.log("Hello Rigo from the console!");
+  var excusa = quien[(Math.floor(Math.random()*(quien.length+1))] +
+  quehizo[(Math.floor(Math.random()*(quehizo.length+1))] +
+  quecosa[(Math.floor(Math.random()*(quecosa.length+1))] +
+  cuando[(Math.floor(Math.random()*(cuando.length+1))];
+
+  console.log(excusa);
+
+
+
+document.querySelector(#excuse).innerHTML = excusa;
+
 };
